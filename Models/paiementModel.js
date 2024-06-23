73dca4ea273c7e136058e3c120138ee301e28e3d
yasmine alaya <alayayesmine12@gmail.com>
2024-06-23 19:10:37 +0100

    paiement Model

--- Models/paiementModel.js
+++ Models/paiementModel.js
@@ -4,8 +4,9 @@ const paiementSchema = new mongoose.Schema({
     commande: {type: mongoose.Schema.Types.ObjectId, ref: 'Commande', required: true},
     montant: {type: Number, required: true},
     dateDePaiement: { type: Date, default: Date.now },
-    methodeDePaiement: {type: String, enum: ['carte bancaire', 'a la livraison'], required:true}
+    methodeDePaiement: {type: String, enum: ['stripe', 'a la livraison'], required:true}
+    //sessionId: {type: String, required: false}
 });
 
-Paiement = mongoose.model('Paiement', paiementSchema);
+const Paiement = mongoose.model('Paiement', paiementSchema);
 module.exports = Paiement;
